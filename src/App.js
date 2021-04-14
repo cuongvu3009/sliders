39c74ff0fa31eb44b71bc1027183f55d4325e7ab
cuongvu3009 <cuongvu3009@gmail.com>
2021-04-14 10:24:04 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import data from "./data";
+import React, { useState } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight onClick={() => setIndex(index + 1)} />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
